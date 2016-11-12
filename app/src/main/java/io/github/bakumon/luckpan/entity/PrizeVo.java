4d9f935bab4c09427fccc9b4753e4fd1189b433b
Bakumon <ma_fei@aliyun.com>
2016-11-12 19:05:38 +0800

    lf

--- app/src/main/java/io/github/bakumon/luckpan/entity/PrizeVo.java
+++ app/src/main/java/io/github/bakumon/luckpan/entity/PrizeVo.java
@@ -1,1 +1,14 @@
-package io.github.bakumon.luckpan.entity;import java.io.Serializable;/** * 奖项实体 * Created by bakumon on 16-11-12. */public class PrizeVo implements Serializable {    public String id;//    public String title;// 奖品名称,奖品名称,    public String rate;// 倍率，大于1的整数,倍率，大于1的整数,}
+package io.github.bakumon.luckpan.entity;
+
+import java.io.Serializable;
+
+/**
+ * 奖项实体
+ * Created by bakumon on 16-11-12.
+ */
+
+public class PrizeVo implements Serializable {
+    public String id;//
+    public String title;// 奖品名称,奖品名称,
+    public String rate;// 倍率，大于1的整数,倍率，大于1的整数,
+}
